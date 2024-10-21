26ff65ad6a5de98ac4d8b2da59d39d71aa16649e
shadab526 <sshadab526@gmail.com>
2024-10-22 04:54:26 +0530

    21 Oct 2024 Task

--- src/BasicsOfJava/Ex_001_Basic.java
+++ src/BasicsOfJava/Ex_001_Basic.java
@@ -2,6 +2,11 @@ package BasicsOfJava;
 
 public class Ex_001_Basic {
     public static void main(String[] args) {
-        System.out.println("Hello Shadab");
+
+        int a = 10;
+        System.out.println(a);
+        int b = a++;
+        System.out.println(b);// Here a value is assigned then a will increment by 1
+        System.out.println(a);
     }
 }
